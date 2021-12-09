c8c3e0b7ef8ecb36b99e92fb13512a3164419fba
Leonardo de Jesus Queiroz <upuaut@tutanota.com>
2021-12-09 19:25:32 -0300

    AT

--- Filtros.m
+++ Filtros.m
@@ -5,12 +5,13 @@ function Historico()
   global qt;
   global i;
   global hist_filt;
-  t = qt;
+  
   
   if(qt == 0)
     warndlg ("Sem historico de operações.");
   else
-      for x = 1:t;
+    t = qt;
+     for x = 1:t;
       list_option{x} = strcat(num2str(x)," - ",hist_filt{x});
       disp(list_option{x});
     endfor
@@ -23,6 +24,33 @@ function Historico()
       qt = sel;
       
       figure(2), subplot(1,1,1);imshow(hist{sel});
+      title(hist_filt{sel});
+    endif
+  endif
+end
+
+
+%%Mostrar Historico
+function Histograma()
+  global hist;
+  global qt;
+  global i;
+  global hist_filt;
+  
+  
+  if(qt == 0)
+    warndlg ("Sem historico de operações.");
+  else
+    t = qt;
+     for x = 1:t;
+      list_option{x} = strcat(num2str(x)," - ",hist_filt{x});
+      disp(list_option{x});
+    endfor
+   
+    [sel, ok] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+
+    if(ok == 1)
+      figure(2000),imhist(hist{sel});
       title(hist_filt{sel});
     endif
   endif
@@ -56,6 +84,34 @@ function ShowI()
   else    
     figure(2), subplot(1,1,1);imshow(i);
     title(hist_filt{qt});
+  endif
+end
+
+%%Mostrar Historico
+function ShowN()
+  global hist;
+  global qt;
+  global i;
+  global hist_filt;
+  global show_select;
+  t = qt;
+  
+  if(t == 0)
+    warndlg ("Sem historico de operações.");
+  else
+      for x = 1:t;
+      list_option{x} = strcat(num2str(x)," - ",hist_filt{x});
+      disp(list_option{x});
+    endfor
+   
+    [sel, ok] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+    disp(sel);
+    disp(ok);
+    if(ok == 1)
+      figure(show_select), subplot(1,1,1);imshow(hist{sel},"papersize", [50, 100]);
+      title(hist_filt{sel});
+      show_select++;
+    endif
   endif
 end
 
@@ -105,7 +161,7 @@ function Limiarizacao()
       endif
       
       i = im2bw(i,limiar);
-      figure, subplot(1,1,1);imshow(i);
+      figure(2), subplot(1,1,1);imshow(i);
       title(strcat ("Limiar =",num2str(limiar)));
       
       %%Armazena historico
@@ -128,7 +184,7 @@ function EscalaCinza()
    if(qt == 0)
     warndlg ("Sem imagem carregada.");
   else
-      if (isrgb(i) == true)
+   
         i = rgb2gray(i);
         figure(2), subplot(1,1,1);imshow(i);
         title("Escala de Cinza");
@@ -137,9 +193,7 @@ function EscalaCinza()
         qt = qt+1;
         hist{qt} = i;
         hist_filt{qt} = "Escala de Cinza";  
-      else
-         warndlg ("Imagem ja esta em Escala de Cinza."); 
-     endif
+    
    endif
 end
 
@@ -315,15 +369,14 @@ function Mediana()
       endif
       disp(mat_t);
       % %Aplica media
-      i = rgb2gray(i);
-      i2 = medfilt2(i,true(mat_t));
+      i = medfilt2(i,true(mat_t));
       
-      figure(2), subplot(1,1,1);imshow(i2);
+      figure(2), subplot(1,1,1);imshow(i);
       title(strcat ("Mediana =",num2str(mat_t)));
       
       %%Armazena historico
       qt = qt+1;
-      hist{qt} = i2;
+      hist{qt} = i;
       hist_filt{qt} = "Mediana";  
       i = i2;
      endif
@@ -366,7 +419,7 @@ function Prewitt()
 end
 
 
-%%Media
+%%PrewittHV
 function PrewittHV()
  global i;
  global hist;
@@ -376,8 +429,7 @@ function PrewittHV()
    if(qt == 0)
     warndlg ("Sem imagem carregada.");
   else
-    
-      
+       
       % %Aplica Prewitt
       h = fspecial('prewitt')
       
@@ -388,12 +440,15 @@ function PrewittHV()
       i = i+i2;
  
       figure(2), subplot(1,1,1);imshow(i);
-      title(strcat ("Prewitt T=",btn));
+      title(strcat ("Prewitt HV"));
       
       %%Armazena historico
+      disp("----");
+      disp(qt);
       qt = qt+1;
+      disp(qt);
       hist{qt} = i;
-      hist_filt{qt} = "Prewitt";  
+      hist_filt{qt} = "Prewitt HV";  
      endif
 end
 
@@ -408,9 +463,7 @@ function Sobel()
    if(qt == 0)
     warndlg ("Sem imagem carregada.");
   else
-         
-  
-      
+
       % %Aplica media
       h = fspecial('sobel');
       
@@ -420,7 +473,6 @@ function Sobel()
       if(strcmp(btn,"Sim"))
           h = h';
       endif
-      
       
       disp(h);
       i = imfilter(i,h);
@@ -437,15 +489,338 @@ end
 
 
 
-% median
-%I = imread('cap.jpeg');
-%I = rgb2gray(I);
-%med= medfilt2(I,true(20));
-%figure(5);
-%imshow(med);
-%title('Mediana');
-%zoom(1.2);
-%% PB(media) e Mediana
+%%SobelHV
+function SobelHV()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+  else
+       
+      % %Aplica Prewitt
+      h = fspecial('sobel')
+      
+      i = imfilter(i,h);
+      i2 = imfilter(i,h');
+      
+      i = i+i2;
+ 
+      figure(2), subplot(1,1,1);imshow(i);
+      title(strcat ("Sobel HV"));
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Sobel HV";  
+     endif
+end
+
+
+
+%%SobelHV
+function SomaIMG()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+   else
+       
+    t = qt;
+ 
+    for x = 1:t;
+    list_option{x} = strcat(num2str(x)," - ",hist_filt{x});
+    disp(list_option{x});
+    endfor
+   
+    [sel1, ok1] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+    [sel2, ok2] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+    
+    
+    if((ok1 + ok2) == 2)
+        Y = hist{sel1};
+        Z = hist{sel2};
+        
+        figure(99),imshow(Y);
+        figure(999),imshow(Z);
+        
+        i = Y+Z;    
+        figure(2), subplot(1,1,1);imshow(i);
+        title("IMG1 + IMG2");
+        disp("----");
+        disp(qt);
+        qt = qt+1;
+        disp(qt);
+        disp("---");
+        hist{qt} = i;
+        hist_filt{qt} = "IMG1 + IMG2";   
+   else
+        warndlg ("É necessario ter duas imagens selecionados.");
+   endif
+  endif
+end
+
+
+%%SobelHV
+function SubIMG()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+   else
+       
+    t = qt;
+ 
+    for x = 1:t;
+    list_option{x} = strcat(num2str(x)," - ",hist_filt{x});
+    disp(list_option{x});
+    endfor
+   
+    [sel1, ok1] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+    [sel2, ok2] = listdlg ("ListString", list_option,"SelectionMode","Single");  
+    
+    
+    if((ok1 + ok2) == 2)
+        Y = hist{sel1};
+        Z = hist{sel2};
+        
+        figure(99),imshow(Y);
+        figure(999),imshow(Z);
+        
+        i = Y-Z;    
+        figure(2), subplot(1,1,1);imshow(i);
+        title("IMG1 - IMG2");
+        disp("----");
+        disp(qt);
+        qt = qt+1;
+        disp(qt);
+        disp("---");
+        hist{qt} = i;
+        hist_filt{qt} = "IMG1 - IMG2";   
+   else
+        warndlg ("É necessario ter duas imagens selecionados.");
+   endif
+  endif
+end
+
+
+%%ROberts
+function Roberts()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+   else
+      % %Aplica media
+      i = edge(i,'roberts');
+  
+      figure(2), subplot(1,1,1);imshow(i);
+      title(strcat ("Roberts"));
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Roberts";  
+     endif
+end
+
+%%Edge
+function Edge()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+  else
+    list = {"roberts","prewitt","sobel","canny","log"};
+   
+    [sel, ok] = listdlg ("ListString", list,"SelectionMode","Single");  
+  
+    if(ok == 1)
+       % %Aplica media
+      i = edge(i,list{sel});
+  
+      figure(2), subplot(1,1,1);imshow(i);
+      title(list{sel});
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = list{sel};  
+      
+    endif
+  endif
+end
+
+%%Media
+function SaltPapper()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+  else
+         
+     %% Cria Caixa de configuração
+     prompt = {"Salt & Papper"};
+     defaults = {"1"};
+     rowscols = [1,10];
+     conf = inputdlg (prompt, "Density", ...
+                     rowscols, defaults);
+     disp(isempty(conf));
+     
+     if(isempty(conf) == 1)
+        dens = 1;
+      else  
+        dens = str2num(conf{1,1});
+      endif
+      % %Aplica ruido
+      i =  imnoise (i, "salt & pepper", dens);
+      
+      figure(2), subplot(1,1,1);imshow(i);
+      title(strcat ("Salt & Papper D=",num2str(dens)));
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Salt & Papper";  
+     endif
+end
+
+
+%%Speckle
+function Speckle()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+  else
+         
+     %% Cria Caixa de configuração
+     prompt = {"Speckle"};
+     defaults = {"1"};
+     rowscols = [1,10];
+     conf = inputdlg (prompt, "Variance", ...
+                     rowscols, defaults);
+     disp(isempty(conf));
+     
+     if(isempty(conf) == 1)
+        varia = 1;
+      else  
+        varia = str2num(conf{1,1});
+      endif
+      % %Aplica ruido
+      i =  imnoise (i, "speckle", varia);
+      
+      figure(2), subplot(1,1,1);imshow(i);
+      title(strcat ("Variance D=",num2str(varia)));
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Variance";  
+     endif
+end
+
+%%poisson
+function Poisson()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+  else
+ 
+      % %Aplica ruido
+      i =  imnoise (i,"poisson");
+      
+      figure(2), subplot(1,1,1);imshow(i);
+      title("Poisson");
+      
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Poisson";  
+     endif
+end
+
+
+%%Reforco Imagem passa Alta
+function Gaussian()
+ global i;
+ global hist;
+ global qt;
+ global hist_filt;
+ 
+   if(qt == 0)
+    warndlg ("Sem imagem carregada.");
+   else
+         
+     %% Cria Caixa de configuração
+     prompt = {"Mean[0,1]","Variance[0.1]"};
+     defaults = {"0.5","0.5"};
+     rowscols = [1,7; 1,7];
+     conf = inputdlg (prompt, "Gaussian", ...
+                     rowscols, defaults);
+    
+    
+     if(isempty(conf) == 1)
+        mean = 0.5;
+        variance =0.5;
+
+      else  
+        mean = str2num(conf{1,1});
+        variance = str2num(conf{2,1});
+        
+      endif  
+    
+
+      i = imnoise(i,"gaussian",mean,variance);
+
+      
+      figure(2), subplot(1,1,1);imshow(i);
+      title(strcat ("Gaussian"," Mean =",num2str(mean),"| Variance =",num2str(variance)));
+
+      %%Armazena historico
+      qt = qt+1;
+      hist{qt} = i;
+      hist_filt{qt} = "Gaussian";  
+     endif
+end
+
+
+
+
+
+
+
+
+##I = imread('rosto.jpeg');
+##figure, imshow(I);
+##title('Original');
+##I = edge(I,'zerocross',0);
+##figure, imshow(I);
+##title('zerocross');
 
 
 
--- Menu.m
+++ Menu.m
@@ -4,7 +4,9 @@ global fnome
 global hist;
 global qt;
 global hist_filt;
+global show_select;
 
+show_select=1000;
 qt = 0;
 
 clear();
@@ -21,29 +23,30 @@ p = uipanel("title", " ▷ Carregar Imagem ◁ ", "position", [.0 .84 1 0.16]);
 p2 = uipanel("title", " ▷ Escolher Filtro ◁ ", "position", [.0 -.013 1 0.85]);
 
 %%Botoes do menu
-b1 = uicontrol("parent", p, "string", "Imagem Original","position", [10 10 155 30],'callback',(@ShowImage));
-b2 = uicontrol("parent", p, "string", "Carregar Arquivo","position",[170 10 155 30],'callback',(@LoadImage));
+b1 = uicontrol("parent", p, "string", "Imagem Original","position", [10 10 150 30],'callback',(@ShowImage));
+b2 = uicontrol("parent", p, "string", "Carregar Arquivo","position",[170 10 150 30],'callback',(@LoadImage));
 b3 = uicontrol("parent", p, "string", "Historico","position",[330 10 150 30],'callback',(@Historico));
-b0 = uicontrol("parent", p, "string", "Show I","position",[485 10 60 30],'callback',(@ShowI));
+b0 = uicontrol("parent", p, "string", "Show N","position",[485 10 60 30],'callback',(@ShowN));
 %Filtros
-b4 = uicontrol("parent", p2, "string", "✩Limiarizaçao ","position",[10 295 155 30],'callback',(@Limiarizacao));
-b5 = uicontrol("parent", p2, "string","✩Escala de Cinza","position", [10 250 155 30],'callback',(@EscalaCinza));
-b6 = uicontrol("parent", p2, "string", "✩Passa Alta-Básico","position",[10 205 155 30],'callback',(@PassaAlta));
-b7 = uicontrol("parent", p2, "string","✩Ref Passa Alta","position", [10 160 155 30],'callback',(@ReforcoPassaAlta));
-b8 = uicontrol("parent", p2, "string","✩Média","position", [10 115 155 30],'callback',(@Media));
-b9 = uicontrol("parent", p2, "string","✩Mediana","position", [10 70 155 30],'callback',(@Mediana));
+b4 = uicontrol("parent", p2, "string", "✩Limiarizaçao ","position",[10 295 150 30],'callback',(@Limiarizacao));
+b5 = uicontrol("parent", p2, "string","✩Escala de Cinza","position", [10 250 150 30],'callback',(@EscalaCinza));
+b6 = uicontrol("parent", p2, "string", "✩Passa Alta-Básico","position",[10 205 150 30],'callback',(@PassaAlta));
+b7 = uicontrol("parent", p2, "string","✩Ref Passa Alta","position", [10 160 150 30],'callback',(@ReforcoPassaAlta));
+b8 = uicontrol("parent", p2, "string","✩Média","position", [10 115 150 30],'callback',(@Media));
+b9 = uicontrol("parent", p2, "string","✩Mediana","position", [10 70 150 30],'callback',(@Mediana));
 
+b9 = uicontrol("parent", p2, "string", "✩Prewitt ","position", [10 26 150 30],'callback',(@Prewitt));
+b10 = uicontrol("parent", p2, "string", "✩Sobel","position",[170 295 150 30],'callback',(@Sobel));
+b11 = uicontrol("parent", p2, "string", "✩PrewittVH","position",[170 250 150 30],'callback',(@PrewittHV));
 
-b9 = uicontrol("parent", p2, "string", "✩Prewitt ","position", [10 26 155 30],'callback',(@Prewitt));
-b10 = uicontrol("parent", p2, "string", "✩Sobel","position",[170 295 155 30],'callback',(@Sobel));
-b11 = uicontrol("parent", p2, "string", "✩PrewittVH","position",[170 10 155 30],'callback',(@PrewittVH));
-
-%b12 = uicontrol("parent", p2, "string", "✩Passa Alta-Refor�o","position",[210 260 155 36],'callback',(i = @PassaAltaReforcada));
-%b13 = uicontrol("parent", p2, "string", "✩Passa Baixa-B�sico","position",[210 220 155 36],'callback',(i = @PassaBaixa));
-%b14 = uicontrol("parent", p2 ,"string","✩Passa Baixa Mediana","position",[210 180 155 36],'callback',(i = @PassaBaixaMediana));
-%b15 = uicontrol("parent", p2, "string","✩Roberts","position", [400 250 155 36],'callback',({@Roberts}));
-%b16 = uicontrol("parent", p2, "string","✩Zerocross","position", [400 90 155 36],'callback',({@Zerocross}));
-%b17 = uicontrol("parent", p2, "string","✩Canny","position", [400 50 155 36],'callback',({@Canny}));
-%b18 = uicontrol("parent", p2, "string","✩Log","position", [400 130 155 36],'callback',(@Log));
-%b19 = uicontrol("parent", p2, "string","✩Histograma Simples","position", [210 90 155 36],'callback',(@histogramaSimples));
+b12 = uicontrol("parent", p2, "string", "✩Sobel HV","position",[170 205 150 30],'callback',(@SobelHV));
+b13 = uicontrol("parent", p2, "string", "✩IMG1 + IMG2","position",[330 295 150 30],'callback',(@SomaIMG));
+b16 = uicontrol("parent", p2, "string","✩IMG1 - IMG2","position", [330 250 150 30],'callback',(@SubIMG));
+b14 = uicontrol("parent", p2 ,"string","✩Roberts","position",[170 115 150 30],'callback',(@Roberts));
+b15 = uicontrol("parent", p2, "string","✩Filt Edge","position", [170 160 150 30],'callback',(@Edge));
+b16 = uicontrol("parent", p2, "string","✩Histograma","position", [330 205 150 30],'callback',(@Histograma));
+b17 = uicontrol("parent", p2, "string","✩Salt & Papper","position", [170 70 150 30],'callback',(@SaltPapper));
+b18 = uicontrol("parent", p2, "string","✩Speckle","position", [170 26 150 30],'callback',(@Speckle));
+b19 = uicontrol("parent", p2, "string","✩Poisson","position", [330 160 150 30],'callback',(@Poisson));
+b19 = uicontrol("parent", p2, "string","✩Gaussian","position", [330 115 150 30],'callback',(@Gaussian));
 
